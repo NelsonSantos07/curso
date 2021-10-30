c160348b43b8f94cfc35ec46fe53f78996e6f6cb
Nelson Silva <neslsonsantostj@hotmail.com>
2021-10-30 14:03:11 -0300

    rest testado

--- src/main/java/com/nelsonsilva/curso/CursoApplication.java
+++ src/main/java/com/nelsonsilva/curso/CursoApplication.java
@@ -1,4 +1,4 @@
-package com.nelsonsilva.cursomc;
+package com.nelsonsilva.curso;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
